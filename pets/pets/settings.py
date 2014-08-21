eba6ce6060dbdd7d17dc913c3b045a53839558d1
aodin <aaronoellis@gmail.com>
2014-08-21 14:05:30 -0600

    Removed secret key from settings

--- pets/pets/settings.py
+++ pets/pets/settings.py
@@ -17,9 +17,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#^+!kr7-shnx#r7k)@rwhe&$z(dd28un#&sl4(0ea60=^-s^8w'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
