f27cc81df827918a35e012b119cf4c987bfac51f
aodin <aaronoellis@gmail.com>
2014-08-25 10:29:33 -0600

    Added case insensitive type filters to random and list views

--- pets/pets/views.py
+++ pets/pets/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from pets.models import Pet    
@@ -9,8 +10,19 @@ def random_pet(request):
     Pull a random pet from the database. The pet's removed field must be NULL.
     Allow filtering through GET parameters.
     """
+    q = Pet.objects.filter(removed__isnull=True)
+    types = request.GET.get('type')
+    if types:
+        where = None
+        for typ in types.split(','):
+            if where:
+                where |= Q(type__icontains=typ)
+            else:
+                where = Q(type__icontains=typ)
+        q = q.filter(where)
+
     try:
-        pet = Pet.objects.filter(removed__isnull=True).order_by('?')[0]
+        pet = q.order_by('?')[0]
     except IndexError:
         pet = None
 
@@ -26,6 +38,15 @@ def all_pets(request):
     List all pets currently available. Allow filtering through GET parameters.
     """
     pets = Pet.objects.filter(removed__isnull=True)
+    types = request.GET.get('type')
+    if types:
+        where = None
+        for typ in types.split(','):
+            if where:
+                where |= Q(type__icontains=typ)
+            else:
+                where = Q(type__icontains=typ)
+        pets = pets.filter(where)
 
     attrs = {
         'pets': pets,
@@ -36,7 +57,7 @@ def all_pets(request):
 
 def single_pet(request, pet_id):
     """
-    Display s ingle pet by id. Allow removed to be displayed.
+    Display a single pet by id. Allow removed pets to be displayed.
     """
     pet = get_object_or_404(Pet, id=pet_id)
 
