b08aff3fc9c6c4d538e48fa1fe7436def2592d53
aodin <aaronoellis@gmail.com>
2014-08-26 21:28:45 -0600

    JSON output of random and all pets views

--- pets/pets/test_views.py
+++ pets/pets/test_views.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*
+import json
+
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
@@ -22,16 +24,21 @@ class ViewTests(TestCase):
         Test the random pet landing page.
         """
         random_url = reverse('random_pet')
-        response = self.client.get(random_url)
-        pet = response.context['pet']
+        resp = self.client.get(random_url)
+        pet = resp.context['pet']
         
         # The pet should not be None
         self.assertTrue(pet)
 
         # Get a specific type, it should be case insensitive
-        response = self.client.get(random_url, {'type': "Cat"})
-        pet = response.context['pet']
-        self.assertEqual(pet.id, "1")        
+        resp = self.client.get(random_url, {'type': "Cat"})
+        pet = resp.context['pet']
+        self.assertEqual(pet.id, "1")
+
+        # Test JSON output
+        resp = self.client.get(random_url, {'type': "Cat", 'format': "JSON"})
+        json_resp = json.loads(resp.content)
+        self.assertEqual(json_resp['id'], "1")
 
 
     def test_all(self):
@@ -39,11 +46,16 @@ class ViewTests(TestCase):
         Test the list view and its filters.
         """
         all_url = reverse('all_pets')
-        response = self.client.get(all_url)
-        pets = response.context['pets']
+        resp = self.client.get(all_url)
+        pets = resp.context['pets']
         self.assertEqual(len(pets), 3)
 
         # Get a specific type, it should be case insensitive
-        response = self.client.get(all_url, {'type': "Cat"})        
-        pets = response.context['pets']
+        resp = self.client.get(all_url, {'type': "Cat"})
+        pets = resp.context['pets']
         self.assertEqual(len(pets), 1)
+
+        # Test JSON output
+        resp = self.client.get(all_url, {'format': "JSON"})
+        json_resp = json.loads(resp.content)
+        self.assertEqual(len(json_resp), 3)
--- pets/pets/views.py
+++ pets/pets/views.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*
+import json
+
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Q
+from django.forms.models import model_to_dict
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 from pets.models import Pet    
+
+
+def json_response(q):
+    """
+    Return a Django queryset as a JSON encoded response.
+    """
+    return HttpResponse(
+        json.dumps(q, cls=DjangoJSONEncoder),
+        content_type="application/json",
+    )
 
 
 def random_pet(request):
@@ -26,10 +41,13 @@ def random_pet(request):
     except IndexError:
         pet = None
 
+    format = request.GET.get('format', "")
+    if format.lower() == "json":
+        return json_response(model_to_dict(pet))
+    
     attrs = {
         'pet': pet,
     }
-
     return render(request, 'random.html', attrs)
 
 
@@ -48,10 +66,13 @@ def all_pets(request):
                 where = Q(type__icontains=typ)
         pets = pets.filter(where)
 
+    format = request.GET.get('format', "")
+    if format.lower() == "json":
+        return json_response([model_to_dict(p) for p in pets])
+    
     attrs = {
         'pets': pets,
     }
-
     return render(request, 'list.html', attrs)
 
 
